96f9c740d0ba2ed72f25b417cc52f29fcf499767
Tolic598 <83219647+Tolic598@users.noreply.github.com>
2022-01-08 12:33:52 +0200

    Update pars.py

--- pars.py
+++ pars.py
@@ -150,7 +150,7 @@ def id(client, message):
 def spam(client, message):
     col=message.command[1]
     text=message.command[2]
-    for i in range(col):
+    for i in range(int(col)):
         message.reply_text(f"{text}")
 
 @app.on_message(filters.command("help",prefixes="/") & filters.me & filters.text)
